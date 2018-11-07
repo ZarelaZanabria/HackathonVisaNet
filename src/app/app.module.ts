dd3acc22b16758e0f67daf4e71846c3a98bd7f41
andrealemonroy <andreale17@icloud.com>
2018-11-07 12:30:51 -0500

    my-nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNavComponent } from './my-nav/my-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent
   ],
   imports: [
     BrowserModule
