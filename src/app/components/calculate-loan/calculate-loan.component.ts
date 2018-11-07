642ec2f9a2cc771c862f3ed611e4abf55e286a05
Elizabeth <elizabethmendoza.unmsm@gmail.com>
2018-11-07 12:20:48 -0500

    Paso 1

---
+++ src/app/components/calculate-loan/calculate-loan.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-loan',
+  templateUrl: './calculate-loan.component.html',
+  styleUrls: ['./calculate-loan.component.scss']
+})
+export class CalculateLoanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+}
