a470a3de7eb446d58ce4a6cf515ec92ff4315620
Zarela Zanabria <zarelixzana@gmail.com>
2018-11-07 12:39:39 -0500

    'RegistroUsuario'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationUserComponent } from './componets/registration-user/registration-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationUserComponent,
   ],
   imports: [
     BrowserModule
