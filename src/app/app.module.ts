f1c894af8176eb5a22b36b6562a74aa5403f2d6d
yulissateran <yulissa.lteran@gmail.com>
2018-11-07 16:13:50 -0500

    agrega maquetación inicial en loan receiver bank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculateLoanComponent } from './components/calculate-loan/calculate-loan.component';
-
+import { LoanReceiverBankComponent} from './components/loan-receiver-bank/loan-receiver-bank.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculateLoanComponent,
+    LoanReceiverBankComponent,
 
   ],
   imports: [
--- src/app/components/loan-receiver-bank/loan-receiver-bank.component.ts
+++ src/app/components/loan-receiver-bank/loan-receiver-bank.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanReceiverBankComponent implements OnInit {
 
-  constructor() { }
-
+  public banks: any[]
+  public validateNumbers : boolean = false;
+  public bankSelect : string;
+  public numberCount: string;
+  private lengthNumbersCount: number;
+  constructor() { 
+    this.banks = [
+      {name: 'Banco de Crédito', numbers:  15},
+      {name: 'Interbank', numbers:  17},
+      {name: 'Banco de la Nación', numbers: 14 },
+      {name: 'BBVA Continental', numbers:  18},
+      {name: 'Banco Pichincha', numbers: 15 },
+      {name: 'Mi banco', numbers: 18},
+      {name: 'Scotiabank', numbers: 20 }, 
+  ];
+ 
+  }
+ 
   ngOnInit() {
+    // this.validateForm()
   }
 
+  validateForm(value1, value2) {
+
+  if(/^([0-9])+$/g.test(this.numberCount) && this.numberCount.length === 1 && value1.value && value2 )
+   this.validateNumbers = true;
+  //  console.log(this.validateNumbers)
+  }
 }
