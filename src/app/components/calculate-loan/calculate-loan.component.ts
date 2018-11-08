5b45ac064270be681cea76a8d1167ef4a58bc2bf
Zarela Zanabria <zarelixzana@gmail.com>
2018-11-08 12:30:03 -0500

    'Style'

--- src/app/components/calculate-loan/calculate-loan.component.ts
+++ src/app/components/calculate-loan/calculate-loan.component.ts
@@ -21,7 +21,7 @@ export class CalculateLoanComponent implements OnInit {
   public arrayBanks: any = [
     { name: 'PRESTAMYPE', img: '', tcea: 12 },
     { name: 'BCP', img: '', tcea: 20 },
-    { name: 'CAJA AREQUIPA', img: '', tcea: 25 }
+    { name: 'CAJA AREQUIPA', img: '', tcea: 25 },
     { name: 'CAJA HUANCAYO', img: '', tcea: 20 },
   ]
 
